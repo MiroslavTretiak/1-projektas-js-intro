517c0504b915fac55b5f1dbc8b6ee5a76970e370
Miroslav <157689323+MiroslavTretiak@users.noreply.github.com>
2024-02-01 13:35:15 +0200

    Boolean

---
+++ data-types/boolean.js
@@ -0,0 +1,44 @@
+/*
+Boolean - loginee reiksme
+
+Galimos reiksmes:
+-true
+-false
+
+Loginiai operatoriai:
+- && (and)
+- || (or)
+
+*/
+
+const arLyja = false;
+const arSninga = true;
+
+const dabartineTemp = -1;
+const siltaNuoTemp = 20;
+
+const arSilta = false;
+
+console.clear();
+
+// Sportas
+// - varzybos vyksta tik dienos metu
+// - vazybos vyksta tik jei turim 2 komandas
+
+
+const arDiena = true; ``
+const arYraDviKomandos = true;
+
+const arVykstaVarzybos = arDiena && arYraDviKomandos;
+console.log('Varzybos:', arVykstaVarzybos);
+
+// Kermosius
+// - vaikas noretu gaidelio
+// - vaikas noretu lazdeles
+// - vaikas noretu grybuko
+
+const arTurimGaidelio = true;
+const arTurimLazdeles = true;
+const arTurimGrybuko = true;
+
+const arVaikasGausSaldaini = 
