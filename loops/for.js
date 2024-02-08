ce97af1fc6d81baf36500d5744be0897a7074a24
Miroslav <157689323+MiroslavTretiak@users.noreply.github.com>
2024-02-08 14:14:10 +0200

    For loop and Array

--- loops/for.js
+++ loops/for.js
@@ -12,3 +12,18 @@ for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 console.log('END');
+
+console.clear();
+
+// ciklas + list
+
+const marks = [10, 2, 8, 4, 6, 10];
+
+let sum = 0;
+
+for (let i = 0; i < marks.length; i++) {
+    console.log(i, marks[i]);
+    sum += marks[i];
+}
+console.log('SUM:', sum, sum / marks.length);
+
