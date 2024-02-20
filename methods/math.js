9cbc51e896e54a6c356b301345ba6500dc1f8f97
Miroslav <157689323+MiroslavTretiak@users.noreply.github.com>
2024-02-20 13:19:31 +0200

    Math.random

--- methods/math.js
+++ methods/math.js
@@ -17,3 +17,68 @@ function findMax(list) {
     return maxNumber;
 }
 console.log(findMax(marks));
+console.clear();
+
+// math.abc nuima - zenkla
+// skrituli apksaiciuot
+console.log(Math.sqrt)
+
+// apvalinimo funkcijos
+const a = 3.1415;
+
+// Math.ceil upp
+console.log(Math.ceil(a))
+
+// Math.round 0.5; 0.49999 down
+console.log(Math.round(a))
+
+// Math.floor down
+console.log(Math.floor(a))
+
+//Math.trunc nupjauna?
+console.log(Math.trunc(a))
+
+console.clear();
+
+// random sukuria skaiciu nuo 0 iki 1 [0, 1)
+
+const r1 = Math.random();
+console.log(r1);
+
+const count = [0, 0];
+
+// funkcijos ideja grazinti nuo 0 iki 1
+
+// function random_0_1() {
+//     return Math.trunc(Math.random() * 2);
+// }
+// for (let i = 0; i < 1000; i++) {
+//     const r = random_0_1();
+//     count[r]++;
+// }
+// console.log(count);
+
+
+// funkcijos ideja grazinti nuo 0 iki 5
+
+// function random_0_5() {
+//     return Math.floor(Math.random() * 5);
+// }
+// const count5 = Array(6).fill(0);
+// for (let i = 0; i < 10; i++) {
+//     const r = random_0_5();
+//     count5[r]++;
+// }
+// console.log(count5);
+
+// min max random n
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const count5 = Array(6).fill(0);
+for (let i = 0; i < 1000; i++) {
+    const min = 2;
+    const r = random(min, 7);
+    count5[r - 2]++;
+}
+console.log(count5);
