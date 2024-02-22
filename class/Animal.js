0cfd1b89f6982ae12a3c351027e04706e1f0d7ad
Miroslav <157689323+MiroslavTretiak@users.noreply.github.com>
2024-02-22 14:35:11 +0200

    Class Animals

---
+++ class/Animal.js
@@ -0,0 +1,14 @@
+
+class Animal {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+        this.breed = 'pet';
+        this.sound = 'Au miau niom';
+    }
+    intro() {
+        return `${this.name}: Hi, i am ${this.color} ${this.breed}. ${this.sound}!!`;
+    }
+}
+
+export default Animal;
